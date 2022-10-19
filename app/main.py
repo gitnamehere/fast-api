2073a8cd06267dc09d2f7656422dd48bc370d2de
jasonh34 <114601751+jh3925@users.noreply.github.com>
2022-10-19 01:33:31 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,8 +3,8 @@ from typing import Union, TYPE_CHECKING
 from fastapi import FastAPI, Depends
 import sqlalchemy.orm.session as Session
 
-from services import get_db
-from models import User
+import services as Services
+import models as Models
 import schemas as Schemas
 
 #Type checking is a way to tell the Python interpreter to check the types of your code.
@@ -13,15 +13,17 @@ if TYPE_CHECKING:
 
 app = FastAPI(title="FastAPI, Docker, OAuth2, and PostgreSQL exercise")
 
-#standard Hello World route
+#standard Hello World
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 #Create a new user
 @app.post("/")
-async def create_user(user: Schemas.CreateUser, db: Session = Depends(get_db)):
-    createUser = User(username=user.username, email=user.email)
+async def create_user(user: Schemas.CreateUser, db: Session = Depends(Services.get_db)):
+
+    #Note: Integrate into services.py later
+    createUser = Models.User(username=user.username, email=user.email)
     db.add(createUser)
     db.commit()
     print("User created")
@@ -29,16 +31,16 @@ async def create_user(user: Schemas.CreateUser, db: Session = Depends(get_db)):
 
 #get a user by id
 @app.get("/users/id/{user_id}")
-async def get_user_by_id(user_id: int, db: Session = Depends(get_db)):
+async def get_user_by_id(user_id: int, db: Session = Depends(Services.get_db)):
     try:
-        return db.query(User).filter(User.id == user_id).first()
+        return db.query(Models.User).filter(Models.User.id == user_id).first()
     except:
         return "User not found or Internal Server Error"
 
 #get every user in the database
 @app.get("/users/all/")
-async def get_all_users(db: Session = Depends(get_db)):
+async def get_all_users(db: Session = Depends(Services.get_db)):
     try:
-        return db.query(User).all()
+        return db.query(Models.User).all()
     except:
         return "Internal Server Error"
