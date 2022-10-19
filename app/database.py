44095239e34ea464847f20f988108477cb0c4fcd
jasonh34 <114601751+jh3925@users.noreply.github.com>
2022-10-18 23:11:32 -0400

    Update file to date with other repo

--- app/database.py
+++ app/database.py
@@ -1,7 +1,8 @@
+#This file is used to reference the database and create a session
+
 import sqlalchemy as SQL
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
 
 #Change (USER) to your username, (PASSWORD) to password, and (DATABASE NAME) to your database name 
 DATABASE_URL = "postgresql://(USER):(PASSWORD)@localhost:5432/(DATABASE NAME)"
