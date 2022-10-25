35484753253bbfe0c558226501bacfa18d337a82
jasonh34 <114601751+jh3925@users.noreply.github.com>
2022-10-25 16:27:53 -0400

    feat: Add example code to frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Formik, FormikProps, Form, Field, FieldProps, useFormik} from 'formik';
+
+
+const FormContainer = () => {
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      password: '',
+      email: ''
+    },
+    onSubmit: values => {
+      alert(JSON.stringify(values, null, 2));
+    }
+  });
+
+  return (
+    <form onSubmit={formik.handleSubmit} className="form">
+      <label htmlFor="email">Email Address</label>
+      <input
+        id="email"
+        name="email"
+        type="email"
+        onChange={formik.handleChange}
+        value={formik.values.email}
+      />
+
+      <label htmlFor="username">Username</label>
+       <input
+         id="username"
+         name="username"
+         type="username"
+         onChange={formik.handleChange}
+         value={formik.values.username}
+       />
+
+       <label htmlFor="password">Password</label>
+       <input
+         id="password"
+         name="password"
+         type="password"
+         onChange={formik.handleChange}
+         value={formik.values.password}
+       />
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Extremely minimalistic form for the purposes of learning, development, testing.</h1>
+      <FormContainer />
     </div>
   );
 }
